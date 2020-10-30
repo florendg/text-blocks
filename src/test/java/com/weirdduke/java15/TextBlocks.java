ded3d54182c5f8ce87dc1f61691bf75e1f195157
Floren de Gier <floren.degier@triodos.com>
2020-10-30 09:09:35 +0100

    Extra scenario

--- src/test/java/com/weirdduke/java15/TextBlocks.java
+++ src/test/java/com/weirdduke/java15/TextBlocks.java
@@ -2,6 +2,8 @@ package com.weirdduke.java15;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.MessageFormat;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TextBlocks {
@@ -10,14 +12,18 @@ class TextBlocks {
     void firstTextBlockExample() {
         String expexted = "Old School\n" +
                 "   multiline String.";
-        String actual= "";
+        String actual= """
+            Old School
+               multiline String.""";
         assertEquals(expexted,actual);
     }
 
     @Test
     void secondTextBlockExample() {
+
         String expected = "SELECT * FROM PERSON " +
                 "WHERE name=\"Duke\"";
+        //language=MySQL
         String actual= """
                 SELECT * FROM PERSON \
                 WHERE name="Duke"\
@@ -26,7 +32,20 @@ class TextBlocks {
     }
 
     @Test
+    void textBlockWithParameters() {
+        String name = "Jane";
+        String expected = "SELECT * FROM PERSON " +
+            "WHERE name=\""+name+"\"";
+        String actual= """
+                SELECT * FROM PERSON \
+                WHERE name="{0}"\
+                """;
+        assertEquals(expected, MessageFormat.format(actual,name));
+    }
+
+    @Test
     void executeJavaScript() {
+        //language=JavaScript
         String script = """
                 function helloDuke(name) {
                     return "Hello " + name;
@@ -35,4 +54,6 @@ class TextBlocks {
                 """;
         assertEquals("Hello Duke", Runner.greet(script,"Duke"));
     }
+
+
 }
